4dd9ddba2bc482e6eb04a86357c1384b9a60fba6
kalenpw <kalenpwilliams@gmail.com>
2017-02-12 00:59:51 -0700

    Updated p164 heap to support patients

---
+++ p164/Heap.cpp
@@ -0,0 +1,168 @@
+//Kalen Williams
+//CS 3385
+//08 Feb. 2017
+//Heap.cpp
+
+#include "./Heap.h"
+#include <iostream>
+#include <sstream>
+// Provides floor, ceil, etc.
+#include <cmath>
+
+using namespace std;
+
+Heap::Heap() {
+  arraySize = 0;
+  n = 0;
+  A = NULL;
+}
+
+// This assumes that every element of the array is an
+// element of the heap.
+Heap::Heap(Patient* inArray, int inArraySize, int inHeapSize) {
+  // TODO: initialize your class data members. An array dynamically allocated
+  // as follows:
+  // A = new int[size];
+  // If you allocate an array like this you MUST deallocate it in your
+  // destructor. This is done for you in the destructor below.
+
+  arraySize = inArraySize;
+  n = inHeapSize;
+  A = new Patient[arraySize];
+  for(int i = 0; i < inHeapSize; i++){
+    A[i] = inArray[i];
+  }
+}
+
+// Destructor. Cleans up memory.
+Heap::~Heap() {
+  delete [] A;
+}
+
+// Note: the function name is prefixed by Heap:: (the class
+// name followed by two colons). Any function defined in
+// the .cpp file must have this prefix.
+
+Patient Heap::at(int i) const{
+    return A[i];
+}
+Patient Heap::parent(int i) const{
+    int parentIndex = (int) (i - 1) / 2;
+    return A[parentIndex];
+}
+
+Patient Heap::left(int i) const {
+    int leftIndex = (i + 1)* 2 - 1;
+    return A[leftIndex];
+}
+
+Patient Heap::right(int i) const {
+    int rightIndex =  (i + 1) * 2;
+    return A[rightIndex];
+}
+//
+//bool Heap::hasLeft(int i) const {
+//    int leftIndex = left(i);
+//    leftIndex += 1;
+//    if(leftIndex > n){
+//        return false;
+//    }
+//    return true;
+//}
+//
+//bool Heap::hasRight(int i) const{
+//    int rightIndex = right(i);
+//    rightIndex += 1;
+//    if(rightIndex > n){
+//        return false;
+//    }
+//    return true;
+//}
+//
+//void Heap::maxHeapify(int i){
+//    int leftIndex = left(i);
+//    int rightIndex = right(i);
+//    int largest = 0;
+//    
+//    if(leftIndex <= n && A[leftIndex] > A[i]){
+//        largest = leftIndex;
+//    }
+//    else{
+//        largest = i;
+//    }
+//
+//    if(rightIndex <= n && A[rightIndex] > A[largest]){
+//        largest = rightIndex;
+//    }
+//
+//    if(largest !=  i){
+//        int swap;
+//        swap = A[largest];
+//        A[largest] = A[i];
+//        A[i] = swap;
+//        maxHeapify(largest); 
+//    }
+//   
+//}
+//
+//void Heap::buildMaxHeap(){
+//    for(int i = n; i >= 0; i--){
+//        maxHeapify(i);
+//    }
+//}
+//
+//
+//
+//bool Heap::operator==(const Heap& rhs) {
+//  if (n != rhs.n) return false;
+//  for (int i = 0; i < n; ++i) {
+//    if (A[i] != rhs.A[i]) return false;
+//  }
+//  return true;
+//}
+//
+//bool Heap::operator==(const int* rhs) {
+//  for (int i = 0; i < n; ++i) {
+//    if (A[i] != rhs[i]) return false;
+//  }
+//  return true;
+//}
+//
+//std::ostream& operator<<(std::ostream& out, const Heap& h) {
+//  out << "[";
+//  for (int i = 0; i < h.n; ++i) {
+//    out << h.A[i];
+//    if (i < h.n-1) {
+//      out << ", ";
+//    }
+//  }
+//  out << "]";
+//  return out;
+//}
+//
+//string toDotImpl(const Heap& h, int i) {
+//  using namespace std;
+//  stringstream ss;
+//  if (h.hasLeft(i)) {
+//    ss << toDotImpl(h, h.left(i));
+//    ss << "\"" << h.at(i) << "\" -> \""
+//       << h.at(h.left(i)) << "\"\n";
+//  }
+//  if (h.hasRight(i)) {
+//    ss << toDotImpl(h, h.right(i));
+//    ss << "\"" << h.at(i) << "\" -> \""
+//       << h.at(h.right(i)) << "\"\n";
+//  }
+//  return ss.str();
+//}
+//
+//string toDot(const Heap& h) {
+//  using namespace std;
+//  stringstream ss;
+//  ss << "digraph G {\n";
+//  ss << "graph [ordering=\"out\"]\n";
+//  ss << "\"" << h.at(0) << "\"\n";
+//  ss << toDotImpl(h, 0);
+//  ss << "}\n";
+//  return ss.str();
+//}
---
+++ p164/Heap.h
@@ -0,0 +1,76 @@
+#pragma once
+
+// Provides I/O
+#include <iostream>
+// Provides size_t
+#include <cstdlib>
+// Provides INT_MAX and INT_MIN
+// You can consider INT_MIN to be negative infinity
+// and INT_MAX to be infinity
+#include <climits>
+
+#include "Patient.h"
+
+//------------------------------------------------------------
+// Heap class
+//------------------------------------------------------------
+class Heap {
+ public:
+  // Constructor
+  Heap();
+
+  // This constructor assumes that every element of the array is an
+  // element of the heap.
+  Heap(Patient* inArray, int inArraySize, int inHeapSize);
+
+  // Destructor
+  ~Heap();
+
+  // Accesses an element of the array.
+  Patient at(int i) const;  
+  // Gets parent index of element at i
+  Patient parent(int i) const;
+
+  // Return element to the  left of i
+  Patient left(int i) const;
+
+  // Return element to the right of i
+  Patient right(int i) const;
+
+  // Checks if an element has a left child
+  bool hasLeft(int i) const;
+
+  // Checks if an elemnt has a right child
+  bool hasRight(int i) const;
+
+  // "Max heapifies" an array
+  void maxHeapify(int i);
+
+  // builds a max heap
+  void buildMaxHeap();
+  
+
+  // Allows comparison between results
+  bool operator==(const Heap& rhs);
+  bool operator==(const int* rhs);
+
+  // Useful for debugging. To use:
+  //   Heap h;
+  //   cout << h << endl;
+  friend std::ostream& operator<<(std::ostream& out, const Heap& h);
+  
+ private:
+  // The array
+  Patient* A;
+
+  // Size of the array
+  int arraySize;
+
+  // The number of elements in the heap
+  int n;
+};
+
+// Useful for debugging. To use:
+//   Heap h;
+//   cout << h << endl;
+std::string toDot(const Heap& h);
