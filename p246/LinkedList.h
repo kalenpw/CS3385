f64351983e31535ec9653b82c94098239325075e
kalenpw <kalenpwilliams@gmail.com>
2017-02-24 02:02:23 -0700

    Single linked list and binary tree working besides remove on SLL and find on BT

--- p246/LinkedList.h
+++ p246/LinkedList.h
@@ -42,24 +42,64 @@ class LinkedList {
     _head = NULL;
   }
 
-  // TODO: implement - set node to _head. If _head is non-null
-  // then update next pointers so that node->next() points to
-  // _head.
+  // Inserts a node to head of linked list
   void insert(ListNode<T>* node) {
+    if(_head == NULL){
+      node->setNext(NULL);
+    } 
+    else{
+      node->setNext(_head);
+    }
+    _head = node;
   }
+    
+    //Returns the node with passed in value key
+    ListNode<T>* find(const T& key) {
+        ListNode<T>* currentNode = _head;
+        while(currentNode != NULL && currentNode->data() != key){
+            currentNode = currentNode->next();  
+        }
+        return currentNode; 
+    }
 
-  // TODO: implement
-  ListNode<T>* find(const T& key) {
-    return NULL;
-  }
+    //Returns the node before the key
+    ListNode<T>* findPreviousNode(const T& key){
+        ListNode<T>* previousNode;
+        ListNode<T>* currentNode = _head;
+    
+        while(currentNode != NULL && currentNode->data() != key){
+            previousNode = currentNode;
+            currentNode = currentNode->next();  
+        }
+        return previousNode;
+    }
 
-  // Removes and deletes the first node in the linked list that has
-  // data equal to key. To delete a node:
-  //
-  //   ListNode<T>* node = ...
-  //   delete node;
-  void remove(const T& key) {
-  }
+    // Removes and deletes the first node in the linked list that has data
+    // equal to the  key
+    void remove(const T& key) {
+        return;
+//        ListNode<T>* previousNode = findPreviousNode(key);
+//        ListNode<T>* deleteNode;
+//        ListNode<T>* nextNode;
+//        if(previousNode->next() == NULL){
+//                        
+//        }
+//
+//        ListNode<T>* nodeToDelete = find(key);
+//        ListNode<T>* previousNode = findPreviousNode(key);
+//        ListNode<T>* nextNode;
+//        if(nodeToDelete == NULL){
+//           nextNode = NULL;
+//           previousNode->setNext(NULL);
+//        }
+//        else{
+//            nextNode = nodeToDelete->next();
+//            previousNode->setNext(nextNode);
+//        }
+////        ListNode<T>* nextNode = nodeToDelete->next();
+//        delete nodeToDelete;
+                
+    }
 
   friend std::ostream& operator<<(std::ostream& out, const LinkedList& list) {
     ListNode<T>* node = list._head;
