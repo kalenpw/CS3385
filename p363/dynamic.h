42a13050aea54d583ad8a55226d253a94c6faa21
kalenpw <kalenpwilliams@gmail.com>
2017-03-14 13:00:09 -0600

    Completed p363 Memoization

--- p363/dynamic.h
+++ p363/dynamic.h
@@ -1,10 +1,11 @@
 #pragma once
-
+#include <climits>
+#include <iostream>
 // Here is the signature for cutRodNaive. The int& means that the
 // parameter is being called by reference. So to increment
 // callcount you will do the following:
 //      callcount++;
-//int cutRodNaive(int n, int& callcount) {
+//int cutRodNaive(const int* p, int n, int& callcount) {
 
 // Here is the signature for cutRodMemoImpl, which is the 
 // implementation function for cut rod using memoization.
@@ -13,4 +14,91 @@
 
 // Here is the signature for cutRodMemo.
 // int cutRodMemo(const int* p, int n, int& callcount) {
+
+void fillArrayWithValue(int* array, int size, int value){
+    for(int i = 0; i <= size; i++){
+        array[i] = value;
+    }
+}
+
+void printArray(int* array, int size){
+    for(int i = 0; i < size; i++){
+        std::cout << array[i] << std::endl;
+    }
+}
+
+int fibNaive(int fibIndex, int& callcount){
+    callcount++;
+    if(fibIndex == 0 || fibIndex == 1){
+        return 1;
+    }
+    return fibNaive(fibIndex - 1, callcount) + fibNaive(fibIndex - 2, callcount);    
+}
+
+int getMax(int first, int second){
+    if(first == second){
+        return first;
+    }
+    else{
+        return first > second ? first : second;
+    }
+}
+
+int cutRodNaive(const int* p, int n, int& callcount){
+    callcount++;
+    if(n == 0){
+        return 0;
+    }
+    int q = INT_MIN;
+    
+    for(int i = 1; i <= n; i++){
+        q = getMax(q, p[i] + cutRodNaive(p, n-i, callcount)); 
+        
+    }
+    return q;
+}
+
+int cutRodMemoImpl(const int* p, int n, int* memos, int& callcount){
+    int q;
+    callcount++;
+    if(memos[n] >= 0){
+        return memos[n];
+    }
+    if(n == 0){
+        q = 0;
+    }
+    else{
+        q = -1;
+        for(int i = 1; i <= n; i++){
+            q = getMax(q, p[i] + cutRodMemoImpl(p, n-i, memos, callcount)); 
+        }
+    }
+    memos[n] = q;
+    return q;
+    
+}
+
+int cutRodMemo(const int* p, int n, int& callcount){
+    int* memos = new int[n+1];
+    fillArrayWithValue(memos, n, -1);
+    return cutRodMemoImpl(p, n, memos, callcount);
+}
+
+//Fib
+int fibMemoImpl(int n, int* memos, int& callcount){
+    callcount++;
+    if(n <= 1){
+        return 1;
+    } 
+    if(memos[n] == -1){
+        memos[n] = fibMemoImpl(n-1, memos, callcount)+fibMemoImpl(n-2,memos,callcount);
+    }
+    return memos[n];
+}
+
+int fibMemo(int n, int& callcount){
+    int* memos = new int[n+1];
+    fillArrayWithValue(memos, n, -1);
+    return fibMemoImpl(n, memos, callcount);
+}
 
--- p363/main.cpp
+++ p363/main.cpp
@@ -31,102 +31,110 @@ int runTests() {
   int numFails = 0;
   int testNum = 0;
 
-  // {
-  //   int callcount = 0;
-  //   int p[] = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
-  //   TEST(cutRodNaive(p, 1, callcount) == 1);
-  //   TEST(cutRodNaive(p, 2, callcount) == 5);
-  //   TEST(cutRodNaive(p, 3, callcount) == 8);
-  //   TEST(cutRodNaive(p, 4, callcount) == 10);
-  //   TEST(cutRodNaive(p, 5, callcount) == 13);
-  //   TEST(cutRodNaive(p, 6, callcount) == 17);
-  //   TEST(cutRodNaive(p, 7, callcount) == 18);
-  //   TEST(cutRodNaive(p, 8, callcount) == 22);
-  //   TEST(cutRodNaive(p, 9, callcount) == 25);
-  //   TEST(cutRodNaive(p, 10, callcount) == 30);
-    
-  //   callcount = 0;
-  //   cutRodNaive(p, 1, callcount);
-  //   TEST(callcount == 2);
+   {
+     int callcount = 0;
+     int p[] = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+     TEST(cutRodNaive(p, 1, callcount) == 1);//1
+     TEST(cutRodNaive(p, 2, callcount) == 5);//2
+     TEST(cutRodNaive(p, 3, callcount) == 8);//3
+     TEST(cutRodNaive(p, 4, callcount) == 10);//4
+     TEST(cutRodNaive(p, 5, callcount) == 13);//5
+     TEST(cutRodNaive(p, 6, callcount) == 17);//6
+     TEST(cutRodNaive(p, 7, callcount) == 18);//7
+     TEST(cutRodNaive(p, 8, callcount) == 22);//8
+     TEST(cutRodNaive(p, 9, callcount) == 25);//9
+     TEST(cutRodNaive(p, 10, callcount) == 30);//10
+  
+     callcount = 0;
+     cutRodNaive(p, 1, callcount);
+     TEST(callcount == 2);//11
 
-  //   callcount = 0;
-  //   cutRodNaive(p, 4, callcount);
-  //   TEST(callcount == 16);
+     callcount = 0;
+     cutRodNaive(p, 4, callcount);
+     TEST(callcount == 16);//12
 
-  //   callcount = 0;
-  //   cutRodNaive(p, 24, callcount);
-  //   TEST(callcount == 16777216);
-  // }
+     callcount = 0;
+     cutRodNaive(p, 24, callcount);
+     TEST(callcount == 16777216);//13
+   }
 
-  // {
-  //   int callcount = 0;
-  //   int p[] = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
-  //   TEST(cutRodMemo(p, 1, callcount) == 1);
-  //   TEST(cutRodMemo(p, 5, callcount) == 13);
-  //   TEST(cutRodMemo(p, 10, callcount) == 30);
-    
-  //   callcount = 0;
-  //   cutRodMemo(p, 1, callcount);
-  //   TEST(callcount == 2);
+   {
+     int callcount = 0;
+     int p[] = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+     TEST(cutRodMemo(p, 1, callcount) == 1);//14
+     TEST(cutRodMemo(p, 5, callcount) == 13);//15
+     TEST(cutRodMemo(p, 10, callcount) == 30);//16
+  
+     callcount = 0;
+     cutRodMemo(p, 1, callcount);//17
+     TEST(callcount == 2);
 
-  //   callcount = 0;
-  //   cutRodMemo(p, 4, callcount);
-  //   TEST(callcount == 11);
+     callcount = 0;
+     cutRodMemo(p, 4, callcount);//18
+     TEST(callcount == 11);
 
-  //   callcount = 0;
-  //   cutRodMemo(p, 24, callcount);
-  //   TEST(callcount == 301);
-  // }
+     callcount = 0;
+     cutRodMemo(p, 24, callcount);
+     TEST(callcount == 301);//19
+   }
 
-  // {
-  //   // The fibonacci sequence is
-  //   //  index: 0  1  2  3  4  5  6  7
-  //   //  value: 1  1  2  3  5  8 13 21
-  //   int callcount = 0;
-  //   TEST(fibNaive(0, callcount) == 1);
-  //   TEST(fibNaive(1, callcount) == 1);
-  //   TEST(fibNaive(2, callcount) == 2);
-  //   TEST(fibNaive(3, callcount) == 3);
-  //   TEST(fibNaive(4, callcount) == 5);
-  //   TEST(fibNaive(5, callcount) == 8);
-    
-  //   callcount = 0;
-  //   fibNaive(0, callcount);
-  //   TEST(callcount == 1);
+   {
+     // The fibonacci sequence is
+     //  index: 0  1  2  3  4  5  6  7
+     //  value: 1  1  2  3  5  8 13 21
+     int callcount = 0;
+     TEST(fibNaive(0, callcount) == 1);//20
+     TEST(fibNaive(1, callcount) == 1);
+     TEST(fibNaive(2, callcount) == 2);
+     TEST(fibNaive(3, callcount) == 3);
+     TEST(fibNaive(4, callcount) == 5);
+     TEST(fibNaive(5, callcount) == 8);//25
+  
+     callcount = 0;
+     fibNaive(0, callcount);
+     TEST(callcount == 1);//26
 
-  //   callcount = 0;
-  //   fibNaive(4, callcount);
-  //   TEST(callcount == 9);
+     callcount = 0;
+     fibNaive(4, callcount);
+     TEST(callcount == 9);//27
 
-  //   callcount = 0;
-  //   fibNaive(34, callcount);
-  //   TEST(callcount == 18454929);
-  // }
+     callcount = 0;
+     fibNaive(34, callcount);
+     TEST(callcount == 18454929);//28
+   }
 
-  // {
-  //   // The fibonacci sequence is
-  //   //  index: 0  1  2  3  4  5  6  7
-  //   //  value: 1  1  2  3  5  8 13 21
-  //   int callcount = 0;
-  //   TEST(fibMemo(0, callcount) == 1);
-  //   TEST(fibMemo(1, callcount) == 1);
-  //   TEST(fibMemo(2, callcount) == 2);
-  //   TEST(fibMemo(3, callcount) == 3);
-  //   TEST(fibMemo(4, callcount) == 5);
-  //   TEST(fibMemo(5, callcount) == 8);
-    
-  //   callcount = 0;
-  //   fibMemo(0, callcount);
-  //   TEST(callcount == 1);
+   {
+     // The fibonacci sequence is
+     //  index: 0  1  2  3  4  5  6  7
+     //  value: 1  1  2  3  5  8 13 21
+     int callcount = 0;
+     TEST(fibMemo(0, callcount) == 1);//29
+     TEST(fibMemo(1, callcount) == 1);//30
+     TEST(fibMemo(2, callcount) == 2);//31
+     TEST(fibMemo(3, callcount) == 3);//32
+     TEST(fibMemo(4, callcount) == 5);//33
+     TEST(fibMemo(5, callcount) == 8);//34
+  
+     callcount = 0;
+     fibMemo(0, callcount);
+     TEST(callcount == 1);//35
+  
+     callcount = 0;
+     fibMemo(4, callcount);
+     TEST(callcount > 1 && callcount < 10);//36
 
-  //   callcount = 0;
-  //   fibMemo(4, callcount);
-  //   TEST(callcount == 7);
+     callcount = 0;
+     fibMemo(34, callcount);
+     TEST(callcount > 30 && callcount < 70); //37
 
-  //   callcount = 0;
-  //   fibMemo(34, callcount);
-  //   TEST(callcount == 67);
-  // }
+     callcount = 0;
+     fibMemo(4, callcount);
+     TEST(callcount == 7);//38
+
+     callcount = 0;
+     fibMemo(34, callcount);
+     TEST(callcount == 67);//39
+   }
 
   const int numSuccesses = testNum - numFails;
   cout << numSuccesses << "/" << testNum << " tests succeeded" << endl;
